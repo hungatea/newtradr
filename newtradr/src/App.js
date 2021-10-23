8d6bf09fa268b8b82a8594405f6d2ac84d3ed85e
hungatea <63873375+hungatea@users.noreply.github.com>
2021-10-23 06:45:12 -0700

    Tested first build

--- newtradr/src/App.js
+++ newtradr/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './newtradr-logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main-container'>
+    <h1>Team SKAD Let's Gooooooo</h1>
     </div>
+    <img src={logo} className='logo' />
+    </>
   );
 }
 
