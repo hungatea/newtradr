1156b7925225d6429084b39844c24b076c2c52d5
hungatea <63873375+hungatea@users.noreply.github.com>
2021-10-23 07:33:32 -0700

    Editing CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ function App() {
   return (
     <>
     <div className='main-container'>
-    <h1>Team SKAD Let's</h1>
+    <div className='q-container'>
+      <h2>How much do you have in savings?</h2>
+      <input className='text-answer' autoFocus type='text' name='name'
+        placeholder='Type your response here...' />
+    </div>
+    <div className='help-text'>
+      <p>Press Enter to continue</p>
+    </div>
     </div>
     <img src={logo} className='logo' />
     </>
