150cc228d467426c47eb2d2a9efd82d57007f1cf
kenny <kennyrobert88@gmail.com>
2021-10-23 10:23:54 -0700

    Update in question part of the probem
    Update question part of the problem in App.js

--- src/App.js
+++ src/App.js
@@ -43,5 +43,50 @@ const sampleQuestion = [
     'Probably too fast for most people',
     "I don't drive"
   ]  
+},
+    'id': 4,
+    'question' : 'How important is it to you that you have a lot of money when you str older?',
+    'type' : 'selectResponse',
+    'layout' : 'column',
+    'options' : [
+      'Very-I aim to have an expensive lifestyle.',
+      'Somewhat-I want to make sure I can live where I want.',
+      'A bit-I do not want to worry about money.',
+      'Not very much-So long as I am doing what makes me happy.'
+  ]
+},
+{
+  'id': 5,
+  'question' : 'How would you describe your typing speed?',
+  'type' : 'selectResponse',
+  'layout' : 'column',
+  'options' : [
+    'To infinity and beyond(more than 100 word per minute)',
+    'Average (between 60 and 100 words per minute)',
+    'Meh (less than 60 words per minute)'
+]
+},
+{
+  'id': 5,
+  'question' : 'How often do you leave work undone until your deadline?',
+  'type' : 'selectResponse',
+  'layout' : 'column',
+  'options' : [
+    'Always.',
+    'Pretty often.',
+    'Never.'
+]
+},
+{
+  'id': 6,
+  'question' : 'Lets just say current stock has a price of $50 per share, and its value has decreased to $42 per share only. If you sel it right now you will ose 8 dollars. 55% o the market analysts say the price will go up to $100 per share 3 months later, and 45% say it will go down evern futher by 10 dollars per month. What is your move here?',
+  'type' : 'selectResponse',
+  'layout' : 'column',
+  'options' : [
+    'Se it now.',
+    'Find someone on the same boat for ideas.',
+    'Wait till after the second month has passed to monitor the price.',
+    'Wait until the price increases.'
+]
 }
 ]
