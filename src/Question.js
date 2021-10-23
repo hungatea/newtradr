e6c937898c0a3fc30da964d3e148d4f42fd17a9d
hungatea <63873375+hungatea@users.noreply.github.com>
2021-10-23 11:05:45 -0700

    jaklsdfjjas

--- src/Question.js
+++ src/Question.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Question(props) {
 
+    const [selected, setSelected] = useState(false);
+    const [answer, setAnswer] = useState('');
+
     const grid = props['question']['layout'] === 'grid';
+
+    function handleKeyPress(event) {
+      if (event.which === 13 && selected) {
+        props.onQuestionChange();
+        setSelected(false);
+      }
+    }
+
+    function handleResponseChange(event) {
+      if (event.target.value !== '') {
+        setSelected(true)
+      } else {
+        setSelected(false);
+      }
+    }
+
+    function handleResponseClick(x) {
+      if (answer !== x) {
+        setAnswer(x);
+      } else {
+        setAnswer('');
+      }
+      setSelected(!selected);
+    }
+
+    useEffect(() => {
+      window.addEventListener("keydown", handleKeyPress);
+      return () => {
+        window.removeEventListener("keydown", handleKeyPress)
+      }
+    })
 
   return (
     <>
@@ -10,18 +44,21 @@ export default function Question(props) {
       <h2>{props['question']['question']}</h2>
     {props['question']['type'] === 'textResponse' &&
     <>
-        <input className='text-answer' autoFocus type='text' name='name' placeholder='Type your response here...' />
+        <input className='text-answer' autoFocus type='text' name='name' onChange={handleResponseChange}
+          placeholder='Type your response here...' />
       </>
-}
+    }
     <div className={`${grid ? 'grid-container' : 'column-container'}`}>
         {props['question']['options'].map(o => {
-            return <div className={`${grid ? 'grid-option' : 'column-option'}`} key={o}>
+            return <div className={`${grid ? 'grid-option' : 'column-option'}`} key={o} onClick={() => {
+              handleResponseClick(o)
+            }}>
                 <p>{o}</p>
                 </div>
         })}
     </div>
 
-    <div className='help-text-container'>
+    <div className={`help-text-container ${selected === false && 'unselected'}`}>
       <div className='help-text'>
       <p style={{marginRight:"8px"}}>Press</p>
       </div>
